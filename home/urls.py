962ce199f74db7cdab7aa3c9ff200c79ea89afb6
farook-heroes <farookheroes@gmail.com>
2023-12-18 19:35:20 +0530

    brand removed

--- home/urls.py
+++ home/urls.py
@@ -4,13 +4,13 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-  path('home/' , views.index,  name='index'),
+  path('' , views.index,  name='index'),
    path('teacher/' , views.teachers,  name='teacher'),
     path('student/' , views.student,  name='student'),
    path('contact/' , views.contact,  name='contact'),
     path('test-email/', views.test_email, name='test_email'),
      path('sample-page/', views.sample_page, name='sample_page'),
-  path('', views.test,  name='test'),
+ 
   path('tables/', views.tables, name='tables'),
   path('calendar/', views.sample_page, name='sample_page'),
   path('tab/', views.tab_page, name='tab'),
