06f169cf75756de8065b621c1de5bc5cca176555
Jagrit Acharya <jagritacharya2020@gmail.com>
2023-12-19 05:42:47 -0700

    host changed

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ DEBUG = 'RENDER' not in os.environ
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Add here your deployment HOSTS
-CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://localhost:5085', 'http://127.0.0.1:8000', 'http://127.0.0.1:5085']
+CSRF_TRUSTED_ORIGINS = ['*']
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
